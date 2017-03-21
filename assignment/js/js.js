2a69469b3232533662a7678009fa5a052c4968f1
giintaare <gintare.jankunaite@gmail.com>
2017-03-21 22:00:25 +0100

    beggining

---
+++ assignment/js/js.js
@@ -0,0 +1,26 @@
+/* blink */
+let dog = document.querySelector("#dog");
+blink.addEventListener('click', startAnimation);
+
+function startAnimation(){
+    dog.classList.add('enable');
+}
+
+/* move */
+let dog = document.querySelector("#dog");
+move.addEventListener('click', startAnimation);
+
+function startAnimation(){
+    dog.classList.add('go');
+}
+
+/* moveto30 */
+/* movefrom30 */
+/* jump */
+/* jumping */
+/* fade */
+/* glow */
+/* fall down */
+/* shake */
+/* roll over */
+
