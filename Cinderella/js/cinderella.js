c935c360c89557243703b857d85cba7d3a7df8bb
giintaare <gintare.jankunaite@gmail.com>
2017-03-30 10:46:30 +0200

    almost finished

---
+++ Cinderella/js/cinderella.js
@@ -0,0 +1,18 @@
+let cinderella = document.querySelector("#cinderella");
+let clock1 = document.querySelector("#clock1");
+let clock2 = document.querySelector("#clock2");
+let prince = document.querySelector("#prince");
+let cinderellashoe = document.querySelector("#cinderellashoe");
+let brownshoe = document.querySelector("#brownshoe");
+let redshoe = document.querySelector("#redshoe");
+let smartshoe = document.querySelector("#smartshoe");
+
+
+function start(){
+    console.log("Event: ready");
+    console.log("Step: start sound");
+    let clocksound = document.querySelector("audio");
+    clocksound.play();
+
+
+}
